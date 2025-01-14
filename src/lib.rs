acded509ea26c556f235a0df0c5c7a51038b4e6d
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-01-14 16:23:03 +0100

    docstrings for lib

--- src/lib.rs
+++ src/lib.rs
@@ -22,13 +22,24 @@ mod preprocessing;
 mod sampling;
 pub mod vector;
 
+/// Maximum number of edges supported by momtrop.
 pub const MAX_EDGES: usize = 64;
+/// Maximum number of vertices supported by momtrop.
 pub const MAX_VERTICES: usize = 256;
 
 #[derive(Debug)]
+/// Struct containing all runtime settings.
 pub struct TropicalSamplingSettings {
+    /// `matrix_stability_test` tests the numerical stability
+    /// of some of the matrix routines used during sampling. This is done by checking
+    /// how far L L^-1 is from the identity matrix in terms of a L_2_1 norm. If this distance is
+    /// larger than the float provided, the sampling will return an Err.
     pub matrix_stability_test: Option<f64>,
+    /// Setting `print_debug_info` to `true` will print out the results of intermediate steps during sampling.
+    /// Useful for debugging.
     pub print_debug_info: bool,
+    /// Enabling `return_metadata` provides access to some of the intermediate results. This is useful for
+    /// exploring new applications.
     pub return_metadata: bool,
 }
 
@@ -53,39 +64,62 @@ pub fn assert_approx_eq<T: MomTropFloat>(res: &T, target: &T, tolerance: &T) {
     }
 }
 
+/// Main graph struct from which a sampler can be build. This graph should be stripped of
+/// tree-like and external edges.
+/// Vertices which would have external edges attached to them must be added to the `externals` field.
 pub struct Graph {
     pub edges: Vec<Edge>,
     pub externals: Vec<u8>,
 }
 
+/// Edge struct from which a graph can be specified.
 pub struct Edge {
     pub vertices: (u8, u8),
     pub is_massive: bool,
+    /// `weight` specifies the power of the propagator corresponding to an edge.
     pub weight: f64,
 }
 
 #[derive(Debug, Clone)]
+/// Return type of the sample function
 pub struct TropicalSampleResult<T: MomTropFloat, const D: usize> {
+    /// loop momenta of sample point.
     pub loop_momenta: Vec<Vector<T, D>>,
+    /// tropical approximation of U at sample point.
     pub u_trop: T,
+    /// tropical approximation of V at sample point.
     pub v_trop: T,
+    /// U at sample point.
     pub u: T,
+    /// V at sample point.
     pub v: T,
+    /// Jacobian at sample point.
     pub jacobian: T,
+    /// metadata, is `None` is `return_metadata` is disabled.
     pub metadata: Option<Metadata<T, D>>,
 }
 
 #[derive(Debug, Clone)]
+/// Optional metadata for advanced users, see \citation for the definition of the fields
+/// in this struct.
 pub struct Metadata<T: MomTropFloat, const D: usize> {
+    /// Vectors q sampled from Gaussian.
     pub q_vectors: Vec<Vector<T, D>>,
+    /// Parameter lambda sampled from Gamma distribution.
     pub lambda: T,
+    /// Matrix L at sample point.
     pub l_matrix: SquareMatrix<T>,
+    /// Cholesky decomposition and inverse of L at sample point.
     pub decompoisiton_result: DecompositionResult<T>,
+    /// The vectors u at sample point.
     pub u_vectors: Vec<Vector<T, D>>,
+    /// L^-1 u at sample point
     pub shift: Vec<Vector<T, D>>,
 }
 
 impl Graph {
+    /// Build the sampler from a graph. `loop_signature` is the signature matrix determining the loop-momentum routing.
+    /// Returns `Err` if a divergent subgraph is found.
     pub fn build_sampler<const D: usize>(
         self,
         loop_signature: Vec<Vec<isize>>,
@@ -109,12 +143,15 @@ fn approx_eq<T: MomTropFloat>(res: &T, target: &T, tolerance: &T) -> bool {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+/// Sampler struct from which sample points can be generated.
 pub struct SampleGenerator<const D: usize> {
     loop_signature: Vec<Vec<isize>>,
     table: TropicalSubgraphTable,
 }
 
 impl<const D: usize> SampleGenerator<D> {
+    /// Generate a sample point for a given random point `x_space_point` in the unit hypercube.
+    /// `edge_data` contains masses and shifs of each propagator.
     pub fn generate_sample_from_x_space_point<
         T: MomTropFloat,
         #[cfg(feature = "log")] L: Logger,
@@ -136,6 +173,8 @@ impl<const D: usize> SampleGenerator<D> {
         )
     }
 
+    /// Alternative to `generate_sample_from_x_space_point`. Uses a `Rng` to generate the point
+    /// in the unit hypercube.
     pub fn generate_sample_from_rng<T: MomTropFloat, R: Rng, #[cfg(feature = "log")] L: Logger>(
         &self,
         edge_data: Vec<(Option<T>, vector::Vector<T, D>)>,
@@ -159,22 +198,27 @@ impl<const D: usize> SampleGenerator<D> {
         )
     }
 
+    /// Dimensionality of the unit hypercube, should match the length of `x_space_point`.
     pub fn get_dimension(&self) -> usize {
         self.table.get_num_variables()
     }
 
+    /// Degree of divergence of the provided graph.
     pub fn get_dod(&self) -> f64 {
         self.table.tropical_graph.dod
     }
 
+    /// Iterate over the edge weights of the provided graph.
     pub fn iter_edge_weights(&self) -> impl Iterator<Item = f64> + '_ {
         self.table.tropical_graph.topology.iter().map(|e| e.weight)
     }
 
+    /// Get the number of edges of the provided graph.
     pub fn get_num_edges(&self) -> usize {
         self.table.tropical_graph.topology.len()
     }
 
+    /// Get the smallest degree of divergence in the subgraph table.
     pub fn get_smallest_dod(&self) -> f64 {
         self.table.get_smallest_dod()
     }
