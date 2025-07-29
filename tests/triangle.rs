a586c14b84cd69f61adf51a2105ad866f1d9c3be
Mathijs Fraaije <mathijsfraaije@gmail.com>
2025-07-29 16:08:53 +0200

    printout to test

--- tests/triangle.rs
+++ tests/triangle.rs
@@ -53,10 +53,12 @@ fn integrate_massless_triangle() {
     let p2 = Vector::from_array([6.0, 7.0, 8.0]);
 
     // setup a random number generator
-    let mut rng = rand::rngs::StdRng::seed_from_u64(42);
+    let mut rng = rand::rngs::StdRng::seed_from_u64(420);
 
     // keep track of the value of the integral.
     let mut sum = 0.0;
+
+    let mut sq_sum = 0.0;
 
     let n_samples = 1000000;
 
@@ -112,7 +114,7 @@ fn integrate_massless_triangle() {
             * energy_1.powf(2. * weight - 1.)
             * energy_2.powf(2. * weight - 1.);
 
-        let pi_factor = (2. * PI).powf(p10.from_isize(3)); // we need to include the the factor (2*pi)^3 for the spatial loop momentum integration.
+        let pi_factor = (2. * PI).powf(3.0); // we need to include the the factor (2*pi)^3 for the spatial loop momentum integration.
         let prefactor = energy_prefactor * sample.jacobian / pi_factor / 8.0; // we add a factor 1/2 for each edge
 
         // now we compute the 6 CFF terms of the triangle
@@ -125,14 +127,24 @@ fn integrate_massless_triangle() {
         let term6 = ((energy_0 + energy_1 - p10) * (energy_1 + energy_2 + p20)).inv();
 
         // The integrand is the sum of the 6 terms multiplied by the prefactor.
-        sum += (term1 + term2 + term3 + term4 + term5 + term6) * prefactor;
+        let value = (term1 + term2 + term3 + term4 + term5 + term6) * prefactor;
+
+        sum += value;
+        sq_sum += value * value;
     }
 
     // compute the average to get an estimate of the integral
-    let avg = sum / p10.from_isize(n_samples as isize);
+    let avg = sum / n_samples as f64;
+    let err = ((sq_sum / n_samples as f64 - avg * avg) / n_samples as f64).sqrt();
 
     // If everything is correct, the result should be approximately 9.76-5
     assert_approx_eq(&avg, &9.758362839019336e-5, &1.0e-2);
+
+    println!();
+    println!("integral: {}", avg);
+    println!("error:    {}", err);
+    println!("target:   {}", 0.0000976546);
+    println!();
 }
 
 // helper functions to compute the onshell energies
